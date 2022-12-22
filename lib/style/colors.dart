59386d0fb5d6efeeed1a3ddfc402543aeac5c55b
mouse0429 <mouse0429@naver.com>
2022-12-22 23:55:02 +0900

    [기타]색상 상수 추가

---
+++ lib/style/colors.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class WakColor {
+  /*Primary colors Gray*/
+  static const grey25 = Color(0xFFFCFCFD);
+  static const grey50 = Color(0xFFF9FAFB);
+  static const grey100 = Color(0xFFF2F4F7);
+  static const grey200 = Color(0xFFE4E7EC);
+  static const grey300 = Color(0xFFD0D5DD);
+  static const grey400 = Color(0xFF98A2B3);
+  static const grey500 = Color(0xFF667085);
+  static const grey600 = Color(0xFF475467);
+  static const grey700 = Color(0xFF344054);
+  static const grey800 = Color(0xFF1D2939);
+  static const grey900 = Color(0xFF101828);
+
+  /*Color Chip Secondary*/
+  static const Color lightBlue = Color(0xFF08DEF7);
+  static const Color yellow = Color(0xFFF4FF81);
+  static const Color purple = Color(0xFF6859EA);
+  static const Color dark = Color(0xFF080F34);
+  static const Color mint = Color(0xFF00F4BA);
+  static const Color blue = Color(0xFF00A3FF);
+  static const Color orange = Color(0xFFFF8F0C); 
+}
