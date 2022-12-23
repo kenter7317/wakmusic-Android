e849cde5c3efe161fc5601cbfcbc83d70f099584
mouse0429 <mouse0429@naver.com>
2022-12-23 23:00:42 +0900

    [에러]fontWeight 세팅 변경

--- lib/style/text_styles.dart
+++ lib/style/text_styles.dart
@@ -2,14 +2,14 @@ import 'package:flutter/material.dart';
 
 class WakText {
   /* light */
-  static final TextStyle txt12L = TextStyle(height: 18/12, fontSize: 12.0, fontWeight: FontWeight.w400, letterSpacing: -0.5, leadingDistribution: TextLeadingDistribution.even);
+  static final TextStyle txt12L = TextStyle(height: 18/12, fontSize: 12.0, fontWeight: FontWeight.w300, letterSpacing: -0.5, leadingDistribution: TextLeadingDistribution.even);
   /* medium */
-  static final TextStyle txt12MH = TextStyle(height: 18/12, fontSize: 12.0, fontWeight: FontWeight.w600, letterSpacing: -0.5, leadingDistribution: TextLeadingDistribution.even);
-  static final TextStyle txt14MH = TextStyle(height: 24/14, fontSize: 14.0, fontWeight: FontWeight.w600, letterSpacing: -0.5, leadingDistribution: TextLeadingDistribution.even);
-  static final TextStyle txt12M = TextStyle(height: 14/12, fontSize: 12.0, fontWeight: FontWeight.w600, letterSpacing: -0.5, leadingDistribution: TextLeadingDistribution.even);
-  static final TextStyle txt14M = TextStyle(height: 20/14, fontSize: 14.0, fontWeight: FontWeight.w600, letterSpacing: -0.5, leadingDistribution: TextLeadingDistribution.even);
-  static final TextStyle txt16M = TextStyle(height: 24/16, fontSize: 16.0, fontWeight: FontWeight.w600, letterSpacing: -0.5, leadingDistribution: TextLeadingDistribution.even);
-  static final TextStyle txt18M = TextStyle(height: 28/18, fontSize: 18.0, fontWeight: FontWeight.w600, letterSpacing: -0.5, leadingDistribution: TextLeadingDistribution.even);
+  static final TextStyle txt12MH = TextStyle(height: 18/12, fontSize: 12.0, fontWeight: FontWeight.w500, letterSpacing: -0.5, leadingDistribution: TextLeadingDistribution.even);
+  static final TextStyle txt14MH = TextStyle(height: 24/14, fontSize: 14.0, fontWeight: FontWeight.w500, letterSpacing: -0.5, leadingDistribution: TextLeadingDistribution.even);
+  static final TextStyle txt12M = TextStyle(height: 14/12, fontSize: 12.0, fontWeight: FontWeight.w500, letterSpacing: -0.5, leadingDistribution: TextLeadingDistribution.even);
+  static final TextStyle txt14M = TextStyle(height: 20/14, fontSize: 14.0, fontWeight: FontWeight.w500, letterSpacing: -0.5, leadingDistribution: TextLeadingDistribution.even);
+  static final TextStyle txt16M = TextStyle(height: 24/16, fontSize: 16.0, fontWeight: FontWeight.w500, letterSpacing: -0.5, leadingDistribution: TextLeadingDistribution.even);
+  static final TextStyle txt18M = TextStyle(height: 28/18, fontSize: 18.0, fontWeight: FontWeight.w500, letterSpacing: -0.5, leadingDistribution: TextLeadingDistribution.even);
   /* bold */
   static final TextStyle txt12B = TextStyle(height: 18/12, fontSize: 12.0, fontWeight: FontWeight.w700, letterSpacing: -0.5, leadingDistribution: TextLeadingDistribution.even);
   static final TextStyle txt16B = TextStyle(height: 24/16, fontSize: 16.0, fontWeight: FontWeight.w700, letterSpacing: -0.5, leadingDistribution: TextLeadingDistribution.even);
