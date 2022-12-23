2fdf901c7dec9c799e9bf52907be87dea6903205
mouse0429 <mouse0429@naver.com>
2022-12-23 22:52:54 +0900

    [에러]fontSize 오류 수정

--- lib/style/text_styles.dart
+++ lib/style/text_styles.dart
@@ -4,10 +4,10 @@ class WakText {
   /* light */
   static final TextStyle txt12L = TextStyle(height: 18/12, fontSize: 12.0, fontWeight: FontWeight.w400, letterSpacing: -0.5, leadingDistribution: TextLeadingDistribution.even);
   /* medium */
-  static final TextStyle txt12MH = TextStyle(height: 18/12, fontSize: 16.0, fontWeight: FontWeight.w600, letterSpacing: -0.5, leadingDistribution: TextLeadingDistribution.even);
-  static final TextStyle txt14MH = TextStyle(height: 24/14, fontSize: 16.0, fontWeight: FontWeight.w600, letterSpacing: -0.5, leadingDistribution: TextLeadingDistribution.even);
-  static final TextStyle txt12M = TextStyle(height: 14/12, fontSize: 16.0, fontWeight: FontWeight.w600, letterSpacing: -0.5, leadingDistribution: TextLeadingDistribution.even);
-  static final TextStyle txt14M = TextStyle(height: 20/14, fontSize: 14.0, fontWeight: FontWeight.w600, letterSpacing: -0.5, leadingDistribution: TextLeadingDistribution.even);
+  static final TextStyle txt12MH = TextStyle(height: 18/12, fontSize: 12.0, fontWeight: FontWeight.w600, letterSpacing: -0.5, leadingDistribution: TextLeadingDistribution.even);
+  static final TextStyle txt14MH = TextStyle(height: 24/14, fontSize: 14.0, fontWeight: FontWeight.w600, letterSpacing: -0.5, leadingDistribution: TextLeadingDistribution.even);
+  static final TextStyle txt12M = TextStyle(height: 14/12, fontSize: 12.0, fontWeight: FontWeight.w600, letterSpacing: -0.5, leadingDistribution: TextLeadingDistribution.even);
+  static final TextStyle txt14M = TextStyle(height: 24/14, fontSize: 14.0, fontWeight: FontWeight.w600, letterSpacing: -0.5, leadingDistribution: TextLeadingDistribution.even);
   static final TextStyle txt16M = TextStyle(height: 24/16, fontSize: 16.0, fontWeight: FontWeight.w600, letterSpacing: -0.5, leadingDistribution: TextLeadingDistribution.even);
   static final TextStyle txt18M = TextStyle(height: 28/18, fontSize: 18.0, fontWeight: FontWeight.w600, letterSpacing: -0.5, leadingDistribution: TextLeadingDistribution.even);
   /* bold */
