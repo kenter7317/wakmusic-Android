5acca3c56bc30842980fb4af73852ad80a9a3fc1
foreverfall123 <foreverfall@naver.com>
2023-01-13 23:27:06 +0900

    [화면] 플레이어 기본 화면 UI 구현

--- lib/screens/player/player_view.dart
+++ lib/screens/player/player_view.dart
@@ -1,8 +1,9 @@
 import 'dart:ui';
 import 'package:audio_video_progress_bar/audio_video_progress_bar.dart';
-import 'package:extended_image/extended_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:wakmusic/style/colors.dart';
+import 'package:wakmusic/widgets/player/player_button.dart';
 
 import '../../style/text_styles.dart';
 
@@ -19,63 +20,85 @@ class Player extends StatelessWidget {
   }
 
   Widget _buildBody(BuildContext context) {
+    var status_height = MediaQuery.of(context).padding.top;
+    var height = MediaQuery.of(context).size.height - status_height;
+    var width = MediaQuery.of(context).size.width;
+
+    var blank = 0;
+    if(height >= 732) {
+      blank = ((height - (width - 50) / (16/9) - 334 - 12) / 20).floor();
+    } else {
+      blank = ((height - (width - 50) / (16/9) - 286 - 12) / 20).floor();
+    }
+
     return Stack(
       children: [
         Positioned(
           left: -83,
           child: Container(
-            height: 300,
+            height: status_height + 45 + 172 + (blank * 4 + 4) - 36,
             width: 540,
-            decoration: BoxDecoration(
+            decoration: const BoxDecoration(
               image: DecorationImage(
                 fit: BoxFit.cover,
-                image: const NetworkImage(
+                image: NetworkImage(
                     'https://i.ytimg.com/vi/lLIpFxWtqCQ/hqdefault.jpg'),
+              ),
+            ),
+            child: ClipRRect(
+              child: BackdropFilter(
+                  filter: ImageFilter.blur(
+                    sigmaX: 200,
+                    sigmaY: 200,
+                  ),
+                  child: Container(
+                    color: Colors.white.withOpacity(0.6),
+                  ),
               ),
             ),
           ),
         ),
-        BackdropFilter(
-          filter: ImageFilter.blur(
-            sigmaX: 100,
-            sigmaY: 100,
-          ),
-          child: Container(
-            color: Colors.white.withOpacity(0.5),
+        _buildPlayer(context, blank),
+        Positioned(
+          top: 8 + MediaQuery.of(context).padding.top,
+          left: 20,
+          height: 32,
+          width: 32,
+          child: GestureDetector(
+            onTap: () => {},
+            child: SvgPicture.asset(
+              'assets/icons/ic_32_arrow_bottom.svg',
+              width: 32,
+              height: 32,
+            ),
           ),
         ),
-        _buildPlayer(context),
       ],
     );
   }
 
-  Widget _buildPlayer(BuildContext context){
-    return SafeArea(
-      child: Column(
-        children: [
-          SizedBox(
-            height: 2,
-          ),
-          _buildTitle(context),
-          SizedBox(
-            height: 54,
-          ),
-          _buildAlbumImage(context),
-          SizedBox(
-            height: 40,
-          ),
-          _buildLyrics(context),
-          SizedBox(
-            height: 43,
-          ),
-          _buildAudioProgressBar(context),
-        ],
-      ),
+  Widget _buildPlayer(BuildContext context, int blank) {
+
+    return Column(
+      children: [
+        SizedBox(height: MediaQuery.of(context).padding.top),
+        _buildTitle(context),
+        SizedBox(height: blank * 4 + 12),
+        _buildAlbumImage(context),
+        SizedBox(height: blank * 4 - 4),
+        _buildLyrics(context),
+        SizedBox(height: blank * 4),
+        _buildAudioProgressBar(context),
+        SizedBox(height: blank * 4),
+        const PlayerButton(),
+      ],
     );
   }
 
-  Widget _buildTitle(BuildContext context){
-    return Center(
+  Widget _buildTitle(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 1.5),
+      child: Center(
         child: Column(
           children: [
             Text(
@@ -84,24 +107,22 @@ class Player extends StatelessWidget {
             ),
             Text(
               '이세계아이돌',
-              style: WakText.txt14M.copyWith(
-                  color: Colors.black.withOpacity(0.60)
-              ),
+              style: WakText.txt14M.copyWith(color: Colors.black.withOpacity(0.60)),
             )
           ],
-        )
+      )),
     );
   }
 
-  Widget _buildAlbumImage(BuildContext context){
+  Widget _buildAlbumImage(BuildContext context) {
     return Container(
-      height: 180,
-      width: 324,
+      height: 172,
+      width: 310,
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(12),
-        image: DecorationImage(
+        image: const DecorationImage(
           fit: BoxFit.cover,
-          image: const NetworkImage(
+          image: NetworkImage(
               'https://i.ytimg.com/vi/lLIpFxWtqCQ/hqdefault.jpg'),
         ),
         /*colorFilter: ColorFilter.mode(
@@ -111,34 +132,29 @@ class Player extends StatelessWidget {
     );
   }
 
-  Widget _buildLyrics(BuildContext context){
+  Widget _buildLyrics(BuildContext context) {
+    var height = MediaQuery.of(context).size.height - MediaQuery.of(context).padding.top;
+    var lyricsList = [
+      '기억나 우리 처음 만난 날',
+      '내게 오던 너의 그 미소가',
+      '마치 날 알고있던 것처럼',
+      '매일 스쳐 지나 가던 바람 처럼',
+      '가끔은 우리 사이가 멀어질까'
+    ];
     return Column(
-      children: [
-        Text(
-          '기억나 우리 처음 만난 날',
-          style: WakText.txt14MH.copyWith(color: WakColor.grey500),
-        ),
-        Text(
-          '내게 오던 너의 그 미소가',
-          style: WakText.txt14MH.copyWith(color: WakColor.grey500),
-        ),
-        Text(
-          '마치 날 알고있던 것처럼',
-          style: WakText.txt14MH.copyWith(color: WakColor.lightBlue),
-        ),
-        Text(
-          '매일 스쳐 지나 가던 바람 처럼',
-          style: WakText.txt14MH.copyWith(color: WakColor.grey500),
-        ),
-        Text(
-          '가끔은 우리 사이가 멀어질까',
-          style: WakText.txt14MH.copyWith(color: WakColor.grey500),
-        ),
-      ],
+      children: List<Widget>.generate(height >= 732 ? 5 : 3, (index) {
+        return SizedBox(
+          height: 24,
+          child: Text(
+            lyricsList[index],
+            style: WakText.txt14MH.copyWith(color: WakColor.grey500),
+          ),
+        );
+      }).toList(),
     );
   }
 
-  Widget _buildAudioProgressBar(BuildContext context){
+  Widget _buildAudioProgressBar(BuildContext context) {
     /*return ValueListenableBuilder(
         valueListenable: ,
         builder: (_, value, __){
@@ -148,10 +164,10 @@ class Player extends StatelessWidget {
         }
     );*/
     return Container(
-      margin: EdgeInsets.symmetric(horizontal: 20),
+      margin: const EdgeInsets.symmetric(horizontal: 20),
       child: Column(
         children: [
-          ProgressBar(
+          const ProgressBar(
             progress: Duration.zero,
             total: Duration(),
             barHeight: 2,
@@ -161,23 +177,19 @@ class Player extends StatelessWidget {
             thumbColor: WakColor.lightBlue,
             timeLabelLocation: TimeLabelLocation.none,
           ),
-          SizedBox(
-            height: 7,
+          const SizedBox(
+            height: 4,
           ),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               Text(
                 '0:00',
-                style: WakText.txt12MH.copyWith(
-                  color: WakColor.lightBlue
-                ),
+                style: WakText.txt12MH.copyWith(color: WakColor.lightBlue),
               ),
               Text(
                 '5:00',
-                style: WakText.txt12MH.copyWith(
-                  color: WakColor.grey400
-                ),
+                style: WakText.txt12MH.copyWith(color: WakColor.grey400),
               ),
             ],
           ),
@@ -186,13 +198,67 @@ class Player extends StatelessWidget {
     );
   }
 
-  Widget _buildPlayerBottomNav(BuildContext context){
+  Widget _buildPlayerBottomNav(BuildContext context) {
     return Container(
-        height: 56,
-        color: Colors.white,
-        child: Row(
-
-        ),
+      height: 56,
+      color: Colors.white,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          InkWell(
+            onTap: () => {},
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SvgPicture.asset('assets/icons/ic_32_heart_on.svg'),
+                Text(
+                  '1.1만',
+                  style: WakText.txt12M.copyWith(color: WakColor.pink),
+                ),
+              ],
+            ),
+          ),
+          InkWell(
+            onTap: () => {},
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SvgPicture.asset('assets/icons/ic_32_views.svg'),
+                Text(
+                  '1.1만',
+                  style: WakText.txt12M.copyWith(color: WakColor.grey400),
+                ),
+              ],
+            ),
+          ),
+          InkWell(
+            onTap: () => {},
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SvgPicture.asset('assets/icons/ic_32_playadd_900.svg'),
+                Text(
+                  '노래담기',
+                  style: WakText.txt12M.copyWith(color: WakColor.grey400),
+                ),
+              ],
+            ),
+          ),
+          InkWell(
+            onTap: () => {},
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SvgPicture.asset('assets/icons/ic_32_play_list.svg'),
+                Text(
+                  '재생목록',
+                  style: WakText.txt12M.copyWith(color: WakColor.grey400),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
---
+++ lib/widgets/player/player_button.dart
@@ -0,0 +1,141 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class RepeatButton extends StatelessWidget {
+  const RepeatButton({Key? key, this.size = 32}) : super(key: key);
+  final double size;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+        onTap: () => {},
+        child: SvgPicture.asset(
+          'assets/icons/ic_32_repeat_off.svg',
+          width: size,
+          height: size,
+        )
+    );
+  }
+}
+
+class PreviousSongButton extends StatelessWidget {
+  const PreviousSongButton({
+    Key? key,
+    this.size = 32,
+  }) : super(key: key);
+  final double size;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+        onTap: () => {},
+        child: SvgPicture.asset(
+          'assets/icons/ic_32_prev_on.svg',
+          width: size,
+          height: size,
+        )
+    );
+  }
+}
+
+class PlayButton extends StatelessWidget {
+  const PlayButton({
+    Key? key,
+    this.size = 32,
+  }) : super(key: key);
+  final double size;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+        onTap: () => {},
+        child: SvgPicture.asset(
+          'assets/icons/ic_32_play_900.svg',
+          width: size,
+          height: size,
+        )
+    );
+  }
+}
+
+class PlayCircleButton extends StatelessWidget {
+  const PlayCircleButton({
+    Key? key,
+    this.size = 100,
+  }) : super(key: key);
+  final double size;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => {},
+      child: SvgPicture.asset(
+        'assets/icons/ic_80_play_shadow.svg',
+        width: 80,
+        height: 80,
+      )
+    );
+  }
+}
+
+
+class NextSongButton extends StatelessWidget {
+  const NextSongButton({Key? key, this.size = 32}) : super(key: key);
+  final double size;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+        onTap: () => {},
+        child: SvgPicture.asset(
+          'assets/icons/ic_32_next_on.svg',
+          width: size,
+          height: size,
+        )
+    );
+  }
+}
+
+class ShuffleButton extends StatelessWidget {
+  const ShuffleButton({Key? key, this.size = 32}) : super(key: key);
+  final double size;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+        onTap: () => {},
+        child: SvgPicture.asset(
+          'assets/icons/ic_32_random_off.svg',
+          width: size,
+          height: size,
+        )
+    );
+  }
+}
+
+class PlayerButton extends StatelessWidget {
+  const PlayerButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: const [
+        RepeatButton(),
+        PreviousSongButton(),
+        PlayCircleButton(),
+        NextSongButton(),
+        ShuffleButton(),
+      ],
+    );
+  }
+}
+
+class PlayerBottomButton extends StatelessWidget {
+  const PlayerBottomButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
