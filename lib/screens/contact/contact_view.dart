2b0fa7f05459a1f66119203af1622d6570deedae
SungyeopJeong <syeopj4a@gmail.com>
2023-05-13 02:06:53 +0900

    [화면]주간차트 영상 문의 화면 완성

--- lib/screens/contact/contact_view.dart
+++ lib/screens/contact/contact_view.dart
@@ -2,19 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:wakmusic/style/colors.dart';
 import 'package:wakmusic/style/text_styles.dart';
-import 'package:wakmusic/widgets/common/dismissible_view.dart';
 import 'package:wakmusic/widgets/common/header.dart';
+import 'package:wakmusic/widgets/common/pop_up.dart';
+import 'package:wakmusic/widgets/show_modal.dart';
 
 enum ContactAbout {
-  bug('버그 제보'),
-  feature('기능 제안'),
-  addSong('노래 추가'),
-  editSong('노래 수정'),
-  chart('주간차트 영상'),
-  select('문의하기');
+  bug('버그 제보', 2),
+  feature('기능 제안', 2),
+  addSong('노래 추가', 4),
+  editSong('노래 수정', 4),
+  chart('주간차트 영상', 1),
+  select('문의하기', 0);
 
-  const ContactAbout(this.btnName);
+  const ContactAbout(this.btnName, this.checkN);
   final String btnName;
+  final int checkN;
 }
 
 class ContactView extends StatefulWidget {
@@ -27,7 +29,46 @@ class ContactView extends StatefulWidget {
 class _ContactViewState extends State<ContactView> {
   ContactAbout _about = ContactAbout.select;
   bool _enable = false;
-  int _selectIdx = ContactAbout.values.length - 1;
+  int _selectIdx = -1;
+  final int _maxFields = 4;
+  late final List<TextEditingController> _fieldTexts;
+  late final List<ScrollController> _scrolls;
+  late final List<FocusNode> _focusNodes;
+  late final List<bool> _checkList;
+
+  @override
+  void initState() {
+    super.initState();
+    _fieldTexts = List.generate(_maxFields, (_) => TextEditingController());
+    _scrolls = List.generate(_maxFields, (_) => ScrollController());
+    _focusNodes = List.generate(
+        _maxFields,
+        (idx) => FocusNode()
+          ..addListener(() {
+            if (!_focusNodes[idx].hasFocus) {
+              String text = '';
+              for (String line in _fieldTexts[idx].text.split('\n')) {
+                if (line.replaceAll(' ', '').isEmpty) continue;
+                text += '$line\n';
+              }
+              if (text.endsWith('\n')) {
+                text = text.substring(0, text.length - 1);
+              }
+              _fieldTexts[idx].text = text;
+            }
+          }));
+    _checkList = List.filled(_maxFields, false);
+  }
+
+  @override
+  void dispose() {
+    for (int i = 0; i < _maxFields; i++) {
+      _fieldTexts[i].dispose();
+      _scrolls[i].dispose();
+      _focusNodes[i].dispose();
+    }
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -39,11 +80,17 @@ class _ContactViewState extends State<ContactView> {
       ContactAbout.chart: _buildChart(),
       ContactAbout.select: _buildSelect(),
     };
-    return DismissibleView(
-      onDismissed: () => Navigator.pop(context),
-      child: Scaffold(
-        backgroundColor: Colors.white,
-        body: SafeArea(
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: GestureDetector(
+          onTap: () {
+            setState(() {
+              for (int i = 0; i < _maxFields; i++) {
+                _focusNodes[i].unfocus();
+              }
+            });
+          },
           child: Column(
             children: [
               Header(
@@ -52,6 +99,7 @@ class _ContactViewState extends State<ContactView> {
               ),
               Expanded(
                 child: SingleChildScrollView(
+                  physics: const BouncingScrollPhysics(),
                   child: Padding(
                     padding: const EdgeInsets.fromLTRB(20, 20, 20, 40),
                     child: widgetMap[_about]!,
@@ -77,7 +125,7 @@ class _ContactViewState extends State<ContactView> {
         ),
         const SizedBox(height: 12),
         SizedBox(
-          height: 48.0 * ((length + 1) ~/ 2) + 8.0 * (length ~/ 2),
+          height: 48.0 * ((length + 1) ~/ 2) + 8.0 * ((length - 1) ~/ 2),
           child: GridView.builder(
             itemCount: length - 1,
             physics: const NeverScrollableScrollPhysics(),
@@ -95,7 +143,6 @@ class _ContactViewState extends State<ContactView> {
                     }),
                     child: Container(
                       padding: const EdgeInsets.all(11),
-                      height: 48,
                       decoration: BoxDecoration(
                         color: Colors.white,
                         border: Border.all(color: WakColor.blue),
@@ -113,8 +160,8 @@ class _ContactViewState extends State<ContactView> {
                           Expanded(
                             child: Text(
                               ContactAbout.values.elementAt(idx).btnName,
-                              style: WakText.txt16M
-                                  .copyWith(color: WakColor.blue),
+                              style:
+                                  WakText.txt16M.copyWith(color: WakColor.blue),
                             ),
                           ),
                           const SizedBox(width: 4),
@@ -134,15 +181,13 @@ class _ContactViewState extends State<ContactView> {
                     }),
                     child: Container(
                       padding: const EdgeInsets.all(11),
-                      height: 48,
                       decoration: BoxDecoration(
                         border: Border.all(color: WakColor.grey200),
                         borderRadius: BorderRadius.circular(12),
                       ),
                       child: Text(
                         ContactAbout.values.elementAt(idx).btnName,
-                        style:
-                            WakText.txt16L.copyWith(color: WakColor.grey900),
+                        style: WakText.txt16L.copyWith(color: WakColor.grey900),
                       ),
                     ),
                   ),
@@ -165,7 +210,90 @@ class _ContactViewState extends State<ContactView> {
   }
 
   Widget _buildChart() {
-    return Column();
+    return Column(
+      children: [
+        _buildForm(
+          formIdx: 0,
+          title: '문의하실 내용을 적어주세요.',
+          hasMaxLine: false,
+        ),
+      ],
+    );
+  }
+
+  Widget _buildForm({
+    required int formIdx,
+    String? title,
+    bool hasMaxLine = true,
+    double maxHeight = 152,
+  }) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        if (title != null)
+          Text(
+            title,
+            style: WakText.txt18M.copyWith(color: WakColor.grey900),
+          ),
+        ConstrainedBox(
+          constraints: BoxConstraints(maxHeight: maxHeight),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16),
+            child: Scrollbar(
+              thickness: hasMaxLine ? 0 : null,
+              child: TextFormField(
+                controller: _fieldTexts[formIdx],
+                focusNode: _focusNodes[formIdx],
+                textInputAction: (hasMaxLine)
+                    ? ((formIdx == _about.checkN - 1)
+                        ? TextInputAction.done
+                        : TextInputAction.next)
+                    : TextInputAction.newline,
+                style: WakText.txt16M.copyWith(color: WakColor.grey600),
+                cursorColor: WakColor.grey900,
+                maxLines: (hasMaxLine) ? 1 : null,
+                autofocus: (formIdx == 0),
+                onTap: () {
+                  setState(() {
+                    _focusNodes[formIdx].requestFocus();
+                  });
+                },
+                onChanged: (value) {
+                  setState(() {
+                    _checkList[formIdx] = value.isNotEmpty;
+                    _enable = _checkList
+                        .sublist(0, _about.checkN)
+                        .every((check) => check);
+                  });
+                },
+                onFieldSubmitted: (value) {
+                  setState(() {
+                    _focusNodes[formIdx].unfocus();
+                    if (formIdx < _about.checkN - 1) {
+                      _focusNodes[formIdx + 1].requestFocus();
+                    }
+                  });
+                },
+                decoration: InputDecoration(
+                  isDense: true,
+                  isCollapsed: true,
+                  border: InputBorder.none,
+                  hintText: '내 답변',
+                  hintStyle: WakText.txt16M.copyWith(color: WakColor.grey400),
+                ),
+              ),
+            ),
+          ),
+        ),
+        Divider(
+          indent: 0,
+          endIndent: 0,
+          color: (_focusNodes[formIdx].hasFocus)
+              ? WakColor.blue
+              : WakColor.grey200,
+        ),
+      ],
+    );
   }
 
   Widget _buildButton() {
@@ -177,6 +305,10 @@ class _ContactViewState extends State<ContactView> {
             if (_enable) {
               setState(() {
                 _about = ContactAbout.values.elementAt(_selectIdx);
+                for (int i = 0; i < _maxFields; i++) {
+                  _fieldTexts[i].clear();
+                }
+                _checkList.fillRange(0, _maxFields, false);
                 _enable = false;
               });
             }
@@ -231,8 +363,26 @@ class _ContactViewState extends State<ContactView> {
             child: GestureDetector(
               onTap: () {
                 if (_enable) {
-                  // call api
-                  Navigator.pop(context);
+                  showModal(
+                    context: context,
+                    builder: (_) => PopUp(
+                      type: PopUpType.txtTwoBtn,
+                      msg: '작성하신 내용으로 등록하시겠습니까?',
+                      posFunc: () => showModal(
+                        context: context,
+                        builder: (_) => const PopUp(
+                          type: PopUpType.txtOneBtn,
+                          msg: '문의가 등록되었습니다.\n도움을 주셔서 감사합니다.',
+                        ),
+                      ).whenComplete(() {
+                        // send inquiry
+                        for (int i = 0; i < _maxFields; i++) {
+                          print('${i}th field: "${_fieldTexts[i].text}"');
+                        }
+                        Navigator.pop(context);
+                      }),
+                    ),
+                  );
                 }
               },
               child: Container(
