773a5babf83cba838c9c7980743a13a3afeaae53
foreverfall123 <foreverfall@naver.com>
2023-05-13 14:10:19 +0900

    [수정] 플레이리스트 곡 순서 변경시에 따로 임시 리스트 만드는 대신 직접적으로 순서 변경

--- lib/screens/player/player_playlist_view.dart
+++ lib/screens/player/player_playlist_view.dart
@@ -74,7 +74,6 @@ class PlayerPlayList extends StatelessWidget {
           :
             GestureDetector(
               onTap: () {
-                viewModel.setList(audioProvider.queue);
                 viewModel.updateStatus(PageState.edit);
               },
               child: const EditBtn(type: BtnType.edit),
@@ -107,8 +106,8 @@ class PlayerPlayList extends StatelessWidget {
                 );
               },
           )
-          : Selector<PlayerPlayListViewModel, List<Song>>(
-              selector: (context, viewModel) => viewModel.playList,
+          : Selector<AudioProvider, List<Song>>(
+              selector: (context, audioProvider) => audioProvider.queue,
               builder: (context, playList, _){
                 return ReorderableListView.builder(
                   proxyDecorator: (child, _, animation) => AnimatedBuilder(
@@ -140,7 +139,8 @@ class PlayerPlayList extends StatelessWidget {
                     tileType: TileType.editTile,
                   ),
                   onReorder: (oldIdx, newIdx) {
-                    viewModel.moveSong(oldIdx, (oldIdx < newIdx) ? newIdx - 1 : newIdx);
+                    Song? song = playList.removeAt(oldIdx);
+                    playList.insert((oldIdx < newIdx) ? newIdx - 1 : newIdx, song);
                   },
                   //onReorderStart: (_) => HapticFeedback.lightImpact(),
                 );
