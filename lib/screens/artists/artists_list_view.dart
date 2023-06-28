f7bb50b57bf9fdb8f96b50fb9f5684e4e4877a61
mouse0429 <mouse0429@naver.com>
2023-06-29 04:10:41 +0900

    [에러]아티스트 리스트 탭 텍스트 멀티라인 현상 해결

--- lib/screens/artists/artists_list_view.dart
+++ lib/screens/artists/artists_list_view.dart
@@ -9,6 +9,7 @@ import 'package:wakmusic/screens/artists/artists_view_model.dart';
 import 'package:wakmusic/style/colors.dart';
 import 'package:wakmusic/style/text_styles.dart';
 import 'package:wakmusic/utils/status_nav_color.dart';
+import 'package:wakmusic/utils/txt_size.dart';
 import 'package:wakmusic/widgets/common/skeleton_ui.dart';
 import 'package:wakmusic/widgets/page_route_builder.dart';
 import 'package:zoom_tap_animation/zoom_tap_animation.dart';
@@ -112,6 +113,7 @@ class ArtistsListView extends StatelessWidget {
   }) {
     ArtistsViewModel viewModel = Provider.of<ArtistsViewModel>(context);
     final calculated = (MediaQuery.of(context).size.width - 56) / 3;
+    final txtMaxWidth = (MediaQuery.of(context).size.width - 34) / 3;
     return Column(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       crossAxisAlignment: CrossAxisAlignment.center,
@@ -155,7 +157,11 @@ class ArtistsListView extends StatelessWidget {
         const SizedBox(height: 4),
         Text(
           artist.name,
-          style: WakText.txt14MH.copyWith(color: WakColor.grey600),
+          style: getTxtSize("김치만두번영택사스가", WakText.txt14MH).width > txtMaxWidth
+              ? (getTxtSize("김치만두번영택사스가", WakText.txt12MH).width > txtMaxWidth
+                  ? WakText.txt11M.copyWith(color: WakColor.grey600)
+                  : WakText.txt12MH.copyWith(color: WakColor.grey600))
+              : WakText.txt14MH.copyWith(color: WakColor.grey600),
           overflow: TextOverflow.visible,
         ),
       ],
