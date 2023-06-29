2a6b673c2e098c06645521485145f9a9284f61dc
mouse0429 <mouse0429@naver.com>
2023-06-29 11:46:34 +0900

    [에러]아티스트 세부 사항 페이지 회색 선 에러 해결

--- lib/screens/artists/artist_detail_view.dart
+++ lib/screens/artists/artist_detail_view.dart
@@ -1,5 +1,6 @@
+import 'dart:math';
+
 import 'package:extended_image/extended_image.dart';
-import 'package:flip_card/flip_card_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:provider/provider.dart';
@@ -14,7 +15,6 @@ import 'package:wakmusic/screens/artists/artists_view_model.dart';
 import 'package:wakmusic/services/apis/api.dart';
 import 'package:wakmusic/style/colors.dart';
 import 'package:wakmusic/style/text_styles.dart';
-import 'package:flip_card/flip_card.dart';
 import 'package:wakmusic/utils/txt_size.dart';
 import 'package:wakmusic/widgets/common/play_btns.dart';
 import 'package:wakmusic/widgets/common/skeleton_ui.dart';
@@ -30,17 +30,54 @@ class ArtistView extends StatefulWidget {
 }
 
 class _ArtistViewState extends State<ArtistView> with TickerProviderStateMixin {
-  late FlipCardController cardController;
   late TabController tabController;
   final ScrollController scrollController = ScrollController();
   final ScrollController descScrollController = ScrollController();
+  bool infoCardFront = true;
   double descScrollHeight = 0;
 
   @override
   void initState() {
     super.initState();
-    cardController = FlipCardController();
     tabController = TabController(length: 3, vsync: this);
+  }
+
+  Widget cardAnimatedBuilder(Widget widget, Animation<double> animation) {
+    final rotate = Tween(begin: pi, end: 0.0).animate(animation);
+
+    return AnimatedBuilder(
+      animation: rotate,
+      child: widget,
+      builder: (_, widget) {
+        final isBack = (ValueKey(infoCardFront) != widget!.key);
+        final value = isBack ? min(rotate.value, pi / 2) : rotate.value;
+
+        var tilt = ((animation.value - 0.5).abs() - 0.5) * 0.0025;
+        tilt *= isBack ? -1.0 : 1.0;
+
+        return Transform(
+          transform: Matrix4.rotationY(value)..setEntry(3, 0, tilt),
+          alignment: Alignment.center,
+          child: widget,
+        );
+      },
+    );
+  }
+
+  Widget artistInfoCardContainer(
+      ValueKey key, double artistImgRatio, Widget widget) {
+    return Container(
+      key: key,
+      height: artistImgRatio * 180,
+      width: artistImgRatio * 187,
+      padding: const EdgeInsets.fromLTRB(16, 16, 8, 12),
+      decoration: BoxDecoration(
+        color: Colors.white.withOpacity(0.4),
+        border: Border.all(color: WakColor.grey25, width: 1),
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: widget,
+    );
   }
 
   @override
@@ -197,140 +234,26 @@ class _ArtistViewState extends State<ArtistView> with TickerProviderStateMixin {
                 width: artistImgRatio * 140,
               ),
               const SizedBox(width: 8),
-              FlipCard(
-                controller: cardController,
-                flipOnTouch: false,
-                direction: FlipDirection.HORIZONTAL,
-                side: CardSide.FRONT,
-                front: Container(
-                  padding: const EdgeInsets.fromLTRB(16, 12, 16, 16),
-                  height: artistImgRatio * 180,
-                  width: artistImgRatio * 187,
-                  decoration: BoxDecoration(
-                    color: Colors.white.withOpacity(0.4),
-                    border: Border.all(color: WakColor.grey25, width: 1),
-                    borderRadius: BorderRadius.circular(8),
-                  ),
-                  child: Stack(
-                    children: [
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          artistInfoCard(context),
-                          Expanded(
-                            child: SingleChildScrollView(
-                              child: Text(
-                                widget.artist.appTitle,
-                                style: WakText.txt14MH
-                                    .copyWith(overflow: TextOverflow.visible),
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                      Positioned(
-                        top: 4,
-                        right: 0,
-                        child: GestureDetector(
-                          onTap: () => cardController.toggleCard(),
-                          child: SvgPicture.asset(
-                            "assets/icons/ic_24_document_off.svg",
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                back: Container(
-                  height: artistImgRatio * 180,
-                  width: artistImgRatio * 187,
-                  padding: const EdgeInsets.fromLTRB(16, 16, 8, 12),
-                  decoration: BoxDecoration(
-                    color: Colors.white.withOpacity(0.4),
-                    border: Border.all(color: WakColor.grey25, width: 1),
-                    borderRadius: BorderRadius.circular(8),
-                  ),
-                  child: Stack(
-                    children: [
-                      Column(
-                        children: [
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Text("소개글", style: WakText.txt16B),
-                              GestureDetector(
-                                onTap: () => cardController.toggleCard(),
-                                child: Padding(
-                                  padding: const EdgeInsets.only(right: 8),
-                                  child: SvgPicture.asset(
-                                    "assets/icons/ic_24_document_on.svg",
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                          const SizedBox(height: 8),
-                          Expanded(
-                            child: NotificationListener(
-                              onNotification: (notification) {
-                                descScrollController.addListener(() {
-                                  setState(() {
-                                    final pos = descScrollController.position;
-                                    descScrollHeight =
-                                        (pos.pixels / pos.maxScrollExtent) *
-                                            (artistImgRatio * 120 - 80);
-                                  });
-                                });
-                                return false;
-                              },
-                              child: SingleChildScrollView(
-                                controller: descScrollController,
-                                scrollDirection: Axis.vertical,
-                                child: Padding(
-                                  padding: const EdgeInsets.only(right: 8),
-                                  child: Text(
-                                    widget.artist.description,
-                                    style: WakText.txt12L.copyWith(
-                                      overflow: TextOverflow.visible,
-                                    ),
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                      Positioned(
-                        bottom: 0,
-                        right: 0,
-                        child: Stack(
-                          children: [
-                            Container(
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(1),
-                                color: WakColor.grey200,
-                              ),
-                              width: 2,
-                              height: artistImgRatio * 120,
-                            ),
-                            Positioned(
-                              top: descScrollHeight,
-                              child: Container(
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(1),
-                                  color: WakColor.grey600,
-                                ),
-                                width: 2,
-                                height: 80,
-                              ),
-                            )
-                          ],
-                        ),
-                      )
-                    ],
-                  ),
+              AnimatedSwitcher(
+                duration: const Duration(milliseconds: 1000),
+                transitionBuilder:
+                    (Widget widget, Animation<double> animation) {
+                  return cardAnimatedBuilder(widget, animation);
+                },
+                layoutBuilder: (widget, list) {
+                  return Stack(
+                    children: [widget!, ...list],
+                  );
+                },
+                child: artistInfoCardContainer(
+                  ValueKey(infoCardFront),
+                  artistImgRatio,
+                  infoCardFront
+                      ? artistInfoCardFront(context)
+                      : artistInfoCardBack(context),
                 ),
               ),
+          
             ],
           ),
         ],
@@ -338,7 +261,129 @@ class _ArtistViewState extends State<ArtistView> with TickerProviderStateMixin {
     );
   }
 
-  Widget artistInfoCard(BuildContext context) {
+  Widget artistInfoCardFront(BuildContext context) {
+    return Stack(
+      children: [
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            artistInfoTxt(context),
+            Expanded(
+              child: SingleChildScrollView(
+                child: Text(
+                  widget.artist.appTitle,
+                  style:
+                      WakText.txt14MH.copyWith(overflow: TextOverflow.visible),
+                ),
+              ),
+            ),
+          ],
+        ),
+        Positioned(
+          top: 4,
+          right: 0,
+          child: GestureDetector(
+            onTap: () {
+              setState(() {
+                infoCardFront = !infoCardFront;
+              });
+            },
+            child: SvgPicture.asset(
+              "assets/icons/ic_24_document_off.svg",
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget artistInfoCardBack(BuildContext context) {
+    double artistImgRatio = (MediaQuery.of(context).size.width - 48) / 327;
+    return Stack(
+      children: [
+        Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text("소개글", style: WakText.txt16B),
+                GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      infoCardFront = !infoCardFront;
+                    });
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.only(right: 8),
+                    child: SvgPicture.asset(
+                      "assets/icons/ic_24_document_on.svg",
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 8),
+            Expanded(
+              child: NotificationListener(
+                onNotification: (notification) {
+                  descScrollController.addListener(() {
+                    setState(() {
+                      final pos = descScrollController.position;
+                      descScrollHeight = (pos.pixels / pos.maxScrollExtent) *
+                          (artistImgRatio * 120 - 80);
+                    });
+                  });
+                  return false;
+                },
+                child: SingleChildScrollView(
+                  controller: descScrollController,
+                  scrollDirection: Axis.vertical,
+                  child: Padding(
+                    padding: const EdgeInsets.only(right: 8),
+                    child: Text(
+                      widget.artist.description,
+                      style: WakText.txt12L.copyWith(
+                        overflow: TextOverflow.visible,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+        Positioned(
+          bottom: 0,
+          right: 0,
+          child: Stack(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(1),
+                  color: WakColor.grey200,
+                ),
+                width: 2,
+                height: artistImgRatio * 120,
+              ),
+              Positioned(
+                top: descScrollHeight,
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(1),
+                    color: WakColor.grey600,
+                  ),
+                  width: 2,
+                  height: 80,
+                ),
+              )
+            ],
+          ),
+        )
+      ],
+    );
+  }
+
+  Widget artistInfoTxt(BuildContext context) {
     double artistImgRatio = (MediaQuery.of(context).size.width - 48) / 327;
     final artist = widget.artist;
     if (artist.id == "woowakgood") {
@@ -364,7 +409,7 @@ class _ArtistViewState extends State<ArtistView> with TickerProviderStateMixin {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           SizedBox(
-            width: artistImgRatio* 150,
+            width: artistImgRatio * 150,
             child: Stack(
               children: [
                 Text(
